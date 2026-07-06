e4742ff2858074efeb316f96cfa520592a1c083f
Andrii Pr <a.v.prykhodko25@gmail.com>
2026-07-07 00:29:34 +0200

    feat: Telegram message templating

---
+++ internal/message/message.go
@@ -0,0 +1,54 @@
+package message
+
+import (
+	"embed"
+	"fmt"
+	"html/template"
+	"strings"
+)
+
+type StreamEvent struct {
+	ChannelID   string
+	ChannelName string
+	Title       string
+	Username    string
+	Game        string
+
+	RecordingURL string
+	Duration     string
+}
+
+var funcMap = template.FuncMap{
+	"trimRedDot": func(s string) string {
+		return strings.TrimPrefix(s, "🔴")
+	},
+}
+
+//go:embed templates/*.gotmpl
+var templateFS embed.FS
+
+var templates = template.Must(template.New("").Funcs(funcMap).ParseFS(templateFS, "templates/*.gotmpl"))
+
+func render(messageType, style, lang string, e StreamEvent) (string, error) {
+	name := fmt.Sprintf("%s_%s_%s.html.gotmpl", messageType, style, lang)
+
+	t := templates.Lookup(name)
+	if t == nil {
+		return "", fmt.Errorf("no template found for %s", name)
+	}
+
+	var buf strings.Builder
+	if err := t.Execute(&buf, e); err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
+}
+
+func FormatLive(style, lang string, e StreamEvent) (string, error) {
+	return render("live", style, lang, e)
+}
+
+func FormatWentOffline(style, lang string, e StreamEvent) (string, error) {
+	return render("offline", style, lang, e)
+}
